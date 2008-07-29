abec7f4ab38ac17e7bf4f254fb6a43288e9a45d8
admc <adam.christian@gmail.com>
2008-07-29 07:52:05 +0000

    Adding a short term solution to the inability to copy the test output
    git-svn-id: http://mozmill.googlecode.com/svn/trunk@73 057c3009-7d4f-0410-8057-1fd5c760e54e

--- mozmill/extension/content/js/glue.js
+++ mozmill/extension/content/js/glue.js
@@ -13,3 +13,32 @@ function runEditor(){
 function genBoiler(){
   utils.genBoiler(window);
 }
+
+function accessOutput(){
+  var copyOutputBox = document.getElementById('copyout');
+  var dx = document.getElementById('dxContainer')
+  var dxDisp = document.getElementById('dxDisplay');
+  
+  //if copyable output is already shown
+  if (copyOutputBox.label == 'Hide Copyable Output'){
+   copyOutputBox.label = 'Show Copyable Output';
+   dx.style.display = 'none';
+   dxDisp.textContent = '';
+   return;
+  }
+  
+  var n = window.document.getElementById('outputtab');
+  var txt = '';
+  for (var c = 0; c < n.childNodes.length; c++){
+    if (n.childNodes[c].textContent){
+      txt += n.childNodes[c].textContent + '\n';  
+    }
+    else{
+      txt += n.childNodes[c].value + '\n';
+    }
+  }
+  dx.style.display = 'block';
+  dxDisp.value = txt;
+  
+  document.getElementById('copyout').label = 'Hide Copyable Output';
+}
