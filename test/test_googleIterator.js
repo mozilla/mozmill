a392ccb2ea21006300e1b63cf648a5b5f4589f78
admc <adam.christian@gmail.com>
2008-07-30 23:08:26 +0000

    Checking in a couple really long tests that just click around all over google/digg
    git-svn-id: http://mozmill.googlecode.com/svn/trunk@85 057c3009-7d4f-0410-8057-1fd5c760e54e

---
+++ test/test_googleIterator.js
@@ -0,0 +1,23 @@
+var elementslib = {}; Components.utils.import('resource://mozmill/modules/elementslib.js', elementslib);
+var mozmill = {}; Components.utils.import('resource://mozmill/modules/mozmill.js', mozmill);
+var results = {}; Components.utils.import('resource://mozmill/modules/results.js', results);
+
+var test_GoogleIterator = function () {
+  // Bring up browser controller.
+  var controller = mozmill.getBrowserController();
+  controller.open('http://www.google.com');
+  controller.waitForElement(new elementslib.Elem( controller.window.content.document.body ));
+  controller.sleep(5000);
+  controller.type(new elementslib.Name(controller.window.content.document, 'q'), 'Mozilla');
+  controller.click(new elementslib.Name(controller.window.content.document, 'btnG'));
+  controller.sleep(2000);
+  var links = controller.window.content.document.getElementsByTagName('a');
+ 
+  for (var i = 0; i<links.length; i++){
+    results.write(links[i].innerHTML);
+    controller.click(new elementslib.Elem( links[i] ));
+    controller.sleep(5000);
+    links = controller.window.content.document.getElementsByTagName('a');
+    controller.sleep(2000);
+  }
+}
