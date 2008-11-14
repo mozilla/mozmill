2aeda9c674e261cedba7806e158e141a112bf0e4
admc <adam.christian@gmail.com>
2008-11-14 23:38:17 +0000

    Checking in some simple UI to get the recorder feature going
    git-svn-id: http://mozmill.googlecode.com/svn/trunk@199 057c3009-7d4f-0410-8057-1fd5c760e54e

---
+++ mozmill/extension/content/rec.js
@@ -0,0 +1,75 @@
+// ***** BEGIN LICENSE BLOCK *****
+// Version: MPL 1.1/GPL 2.0/LGPL 2.1
+// 
+// The contents of this file are subject to the Mozilla Public License Version
+// 1.1 (the "License"); you may not use this file except in compliance with
+// the License. You may obtain a copy of the License at
+// http://www.mozilla.org/MPL/
+// 
+// Software distributed under the License is distributed on an "AS IS" basis,
+// WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+// for the specific language governing rights and limitations under the
+// License.
+// 
+// The Original Code is Mozilla Corporation Code.
+// 
+// The Initial Developer of the Original Code is
+// Adam Christian.
+// Portions created by the Initial Developer are Copyright (C) 2008
+// the Initial Developer. All Rights Reserved.
+// 
+// Contributor(s):
+//  Adam Christian <adam.christian@gmail.com>
+//  Mikeal Rogers <mikeal.rogers@gmail.com>
+// 
+// Alternatively, the contents of this file may be used under the terms of
+// either the GNU General Public License Version 2 or later (the "GPL"), or
+// the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+// in which case the provisions of the GPL or the LGPL are applicable instead
+// of those above. If you wish to allow use of your version of this file only
+// under the terms of either the GPL or the LGPL, and not to allow others to
+// use your version of this file under the terms of the MPL, indicate your
+// decision by deleting the provisions above and replace them with the notice
+// and other provisions required by the GPL or the LGPL. If you do not delete
+// the provisions above, a recipient may use your version of this file under
+// the terms of any one of the MPL, the GPL or the LGPL.
+// 
+// ***** END LICENSE BLOCK *****
+
+var inspection = {}; Components.utils.import('resource://mozmill/modules/inspection.js', inspection);
+var utils = {}; Components.utils.import('resource://mozmill/modules/utils.js', utils);
+
+var RecorderConnector = function() {
+  this.lastEvent = null;
+}
+
+RecorderConnector.prototype.toggle = function(){
+  if ($('recorder').getAttribute('label') ==  'Stop'){
+    this.off();
+  }
+  else{
+    this.on();
+  }
+};
+
+RecorderConnector.prototype.on = function() {
+  $('recorder').setAttribute('label', 'Stop');
+  var mmWindows = utils.getWindows('navigator:browser');
+  if (mmWindows.length != 0){
+    mmWindows[0].focus();
+  }
+};
+
+RecorderConnector.prototype.off = function() {
+  $('recorder').setAttribute('label', 'Record');
+};
+
+var MozMillrec = new RecorderConnector();
+
+// Scoping bug workarounds
+var enableRec = function () {
+  MozMillrec.on();
+}
+var disableRec = function () {
+  MozMillrec.off();
+}
