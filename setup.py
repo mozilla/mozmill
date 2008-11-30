042e1975663ab2a962486041051eecdc7973e33a
mikeal <mikeal.rogers@gmail.com>
2008-11-30 00:06:38 +0000

    Fixing package data include.
    git-svn-id: http://mozmill.googlecode.com/svn/trunk@222 057c3009-7d4f-0410-8057-1fd5c760e54e

--- setup.py
+++ setup.py
@@ -52,7 +52,8 @@ setup(name=PACKAGE_NAME,
       url='http://code.google.com/p/mozmill',
       license='http://www.apache.org/licenses/LICENSE-2.0',
       packages=find_packages(exclude=['test']),
-      package_data = {'': ['*.js', '*.css', '*.html', '*.txt', '*.xpi', '*.rdf', '*.xul', '*.jsm', '*.xml' ],},
+      include_package_data = True,
+      package_data = {'': ['*.js', '*.css', '*.html', '*.txt', '*.xpi', '*.rdf', '*.xul', '*.jsm', '*.xml' 'extension'],},
       entry_points="""
           [console_scripts]
           mozmill = mozmill:main
