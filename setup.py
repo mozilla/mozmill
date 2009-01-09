a0f49f825e9a90a8db1a0403819545033c7876af
mikeal <mikeal.rogers@gmail.com>
2009-01-09 22:23:44 +0000

    Getting ready for release.
    git-svn-id: http://mozmill.googlecode.com/svn/trunk@281 057c3009-7d4f-0410-8057-1fd5c760e54e

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ desc = """UI Automation tool for Mozilla applications."""
 summ = """A tool for full UI automation of Mozilla applications."""
 
 PACKAGE_NAME = "mozmill"
-PACKAGE_VERSION = "1.0rc3"
+PACKAGE_VERSION = "1.0"
 
 setup(name=PACKAGE_NAME,
       version=PACKAGE_VERSION,
