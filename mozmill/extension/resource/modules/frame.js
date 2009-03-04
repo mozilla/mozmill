abb1d3ce44ce540a7096d5c4b996cbd528981d66
mikeal <mikeal.rogers@gmail.com>
2009-03-04 21:42:31 +0000

    Fixing exception when metadata is not properly defined.
    git-svn-id: http://mozmill.googlecode.com/svn/trunk@408 057c3009-7d4f-0410-8057-1fd5c760e54e

--- mozmill/extension/resource/modules/frame.js
+++ mozmill/extension/resource/modules/frame.js
@@ -450,7 +450,9 @@ Runner.prototype._runTestModule = function (module) {
 Runner.prototype.runTestModule = function (module) {
   if (module.__requirements__ != undefined) {
     if (!arrays.inArray(this.collector.loaded_directories, module.__root_path__)) {
-      this.collector.initTestDirectory(module.__root_path__);
+      if (module.__root_path__ != undefined) {
+        this.collector.initTestDirectory(module.__root_path__);
+      }
     }
     var deps = this.getDependencies(module);
     for (i in deps) {
