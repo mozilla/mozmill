8b36ac38d441e59f0b829478b45ac4d91c4beb1c
admc <adam.christian@gmail.com>
2009-06-16 01:52:57 +0000

    backing out the waitForPageload because it stalls on pages that load too fast
    git-svn-id: http://mozmill.googlecode.com/svn/trunk@496 057c3009-7d4f-0410-8057-1fd5c760e54e

--- mozmill/extension/resource/modules/controller.js
+++ mozmill/extension/resource/modules/controller.js
@@ -710,11 +710,13 @@ var waitFor = function (node, events) {
   }
   this.events = events;
   this.node = node;
-  node.firedEvents = {}
   this.registry = {};
+  if (node._mozmillEventRegistry == undefined) {
+    node._mozmillEventRegistry = {}
+  }
   for each(e in events) {
     var listener = function (event) {
-      this.firedEvents[event.type] = true;
+      this.result = true;
     }
     this.registry[e] = listener;
     this.registry[e].result = false;
@@ -723,7 +725,7 @@ var waitFor = function (node, events) {
 }
 waitFor.prototype.wait = function (timeout, interval) {
   for (e in this.registry) {
-    var r = waitForEval("subject['"+e+"'] == true", timeout, interval, this.node.firedEvents)
+    var r = waitForEval("subject.result == true", timeout, interval, this.registry[e])
     if (!r) {
         throw "Event didn't fire before timeout. event == "+e+", result is "+this.registry[e].result;
       }
@@ -800,14 +802,10 @@ function browserAdditions( controller ) {
     if (timeout == undefined) {
       var timeout = 30000;
     }
-    
-    //Wait for the state where the page has unloaded
-    //then wait for the new one to load
-    waitForEval("subject.body.style == undefined", timeout, interval, _document);
     waitForEval("subject.body.style != undefined", timeout, interval, _document);
     //Once the object is available it's somewhere between 1 and 3 seconds before the DOM
     //Actually becomes available to us
-    //sleep(3000);
+    sleep(3000);
   }
 }
 
