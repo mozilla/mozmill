f8c23a9def589dd5515b4f9da48f8d192aadae0d
whimboo <hskupin@mozilla.com>
2009-11-04 21:49:08 +0100

    Check if element exists before trying to access the innerHTML property

--- mozmill/extension/resource/modules/controller.js
+++ mozmill/extension/resource/modules/controller.js
@@ -625,7 +625,7 @@ MozMillController.prototype.assertText = function (el, text) {
   //this.window.focus();
   var n = el.getNode();
 
-  if (n.innerHTML == text){
+  if (n && n.innerHTML == text){
     frame.events.pass({'function':'Controller.assertText()'});
     return true;
    }
