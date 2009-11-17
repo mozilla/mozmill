64f173447341bcac6a01ee8343061595d04e6678
Mikeal Rogers <mikeal.rogers@gmail.com>
2009-11-17 15:27:33 -0800

    Rolling version for release.

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ desc = """UI Automation tool for Mozilla applications."""
 summ = """A tool for full UI automation of Mozilla applications."""
 
 PACKAGE_NAME = "mozmill"
-PACKAGE_VERSION = "1.3beta1"
+PACKAGE_VERSION = "1.3"
 
 setup(name=PACKAGE_NAME,
       version=PACKAGE_VERSION,
