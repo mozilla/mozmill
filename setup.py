47214ee332a7fc0a88c06bc8f04623f77eb32592
Mikeal Rogers <mikeal.rogers@gmail.com>
2010-01-19 16:51:51 -0800

    Forcing mozrunner update.

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(name=PACKAGE_NAME,
           mozmill-restart = mozmill:restart_cli
         """,
       platforms =['Any'],
-      install_requires = ['jsbridge >= 2.3.4', 'mozrunner >= 2.3.1'],
+      install_requires = ['jsbridge >= 2.3.4', 'mozrunner >= 2.4'],
       classifiers=['Development Status :: 4 - Beta',
                    'Environment :: Console',
                    'Intended Audience :: Developers',
