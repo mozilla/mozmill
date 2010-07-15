a49b950b9d01dd6d537c3987fb2212860039d2d1
ctalbert <ctalbert@mozilla.com>
2010-07-15 10:50:36 -0700

    checking on the workflow on the wiki page
    testing change only -- change2
    
    Changing version numbers in setup.py to match 1.4.2 branch

--- jsbridge/setup.py
+++ jsbridge/setup.py
@@ -42,9 +42,9 @@ desc = """Python to JavaScript bridge interface."""
 summ = """A powerful and extensible Python to JavaScript bridge interface."""
 
 PACKAGE_NAME = "jsbridge"
-PACKAGE_VERSION = "2.3.6"
+PACKAGE_VERSION = "2.3.7b1"
 
-requires = ['mozrunner >= 2.4.2']
+requires = ['mozrunner >= 2.4.4b1']
 
 if not sys.version.startswith('2.6'):
     requires.append('simplejson')
