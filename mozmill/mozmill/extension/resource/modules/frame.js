b8c9697fd259a44f929b3291f0bf3e1eec579184
whimboo <hskupin@mozilla.com>
2010-07-28 13:22:13 +0200

    Bug 581329 - Use local variables in for loops instead of global ones. r=jhammel

--- mozmill/mozmill/extension/resource/modules/frame.js
+++ mozmill/mozmill/extension/resource/modules/frame.js
@@ -121,7 +121,7 @@ function stateChangeBase (possibilties, restrictions, target, cmeta, v) {
     } 
   }
   if (restrictions) {
-    for (i in restrictions) {
+    for (var i in restrictions) {
       var r = restrictions[i];
       if (!r(v)) {
         // TODO error value did not pass restriction
@@ -194,7 +194,7 @@ events.pass = function (obj) {
   if (events.currentTest) {
     events.currentTest.__passes__.push(obj);
   }
-  for each(timer in timers) {
+  for each(var timer in timers) {
     timer.actions.push(
       {"currentTest":events.currentModule.__file__+"::"+events.currentTest.__name__, "obj":obj,
        "result":"pass"}
@@ -206,7 +206,7 @@ events.fail = function (obj) {
   if (events.currentTest) {
     events.currentTest.__fails__.push(obj);
   }
-  for each(time in timers) {
+  for each(var time in timers) {
     timer.actions.push(
       {"currentTest":events.currentModule.__file__+"::"+events.currentTest.__name__, "obj":obj,
        "result":"fail"}
@@ -217,7 +217,7 @@ events.fail = function (obj) {
 events.skip = function (reason) {
   events.currentTest.skipped = true;
   events.currentTest.skipped_reason = reason;
-  for each(timer in timers) {
+  for each(var timer in timers) {
     timer.actions.push(
       {"currentTest":events.currentModule.__file__+"::"+events.currentTest.__name__, "obj":reason,
        "result":"skip"}
@@ -230,11 +230,11 @@ events.fireEvent = function (name, obj) {
     throw new Error("tests that use firePythonCallback cannot be run from the IDE\n");
   }
   if (this.listeners[name]) {
-    for (i in this.listeners[name]) {
+    for (var i in this.listeners[name]) {
       this.listeners[name][i](obj);
     }
   }
-  for each(listener in this.globalListeners) {
+  for each(var listener in this.globalListeners) {
     listener(name, obj);
   }
 }
@@ -249,15 +249,15 @@ events.addListener = function (name, listener) {
   }
 }
 events.removeListener = function(listener) {
-  for (listenerIndex in this.listeners) {
+  for (var listenerIndex in this.listeners) {
     var e = this.listeners[listenerIndex];
-    for (i in e){
+    for (var i in e){
       if (e[i] == listener) {
         this.listeners[listenerIndex] = arrayRemove(e, i);
       }
     }
   }
-  for (i in this.globalListeners) {
+  for (var i in this.globalListeners) {
     if (this.globalListeners[i] == listener) {
       this.globalListeners = arrayRemove(this.globalListeners, i);
     }
@@ -294,9 +294,11 @@ function Collector () {
   // var logging = {}; Components.utils.import('resource://mozmill/stdlib/logging.js', logging);
   // this.logger = new logging.Logger('Collector');
 }
+
 Collector.prototype.getModule = function (name) {
   return this.test_modules_by_name[name];
 }
+
 Collector.prototype.startHttpd = function () {
   while (this.httpd == undefined) {
     try {
@@ -336,7 +338,7 @@ Collector.prototype.addHttpResource = function (directory, ns) {
 Collector.prototype.initTestModule = function (filename) {
   var test_module = loadFile(filename, this);
   test_module.__tests__ = [];
-  for (i in test_module) {
+  for (var i in test_module) {
     if (test_module[i] == null) {
       // do nothing
     }
@@ -376,7 +378,7 @@ Collector.prototype.initTestModule = function (filename) {
   }
   
   if (test_module.MODULE_REQUIRES != undefined && test_module.RELATIVE_ROOT == undefined) {
-    for each(t in test_module.__tests__) {
+    for each(var t in test_module.__tests__) {
       t.__force_skip__ = "RELATIVE ROOT is not defined and test requires another module.";
     }
   }
@@ -384,14 +386,16 @@ Collector.prototype.initTestModule = function (filename) {
   test_module.collector = this;
   test_module.status = 'loaded';
   this.test_modules_by_filename[filename] = test_module;
+
   return test_module;
 }
+
 Collector.prototype.initTestDirectory = function (directory) {
   var r = this;
   function recursiveModuleLoader(dfile) {
     r.loaded_directories.push(directory);
     var dfiles = os.listDirectory(dfile);
-    for (i in dfiles) {
+    for (var i in dfiles) {
       var f = dfiles[i];
       if ( f.isDirectory() && 
            !withs.startsWith(f.leafName, '.') && 
@@ -421,7 +425,7 @@ function Runner (collector, invokedFromIDE) {
 Runner.prototype.runTestDirectory = function (directory) {
   this.collector.initTestDirectory(directory);
   
-  for (i in this.collector.test_modules_by_filename) {
+  for (var i in this.collector.test_modules_by_filename) {
     var test = this.collector.test_modules_by_filename[i];
     if (test.status != 'done') {
       this.runTestModule(test);
@@ -447,7 +451,7 @@ Runner.prototype.getDependencies = function (module) {
   events.setState('dependencies');
   var alldeps = [];
   function recursiveGetDeps (mod) {
-    for (i in mod.__dependencies__) {
+    for (var i in mod.__dependencies__) {
       var m = mod.dependencies[i];
       if ( !arrays.inArray(this.test_modules_by_name, m) ) {
         // TODO: Raise Error that this dependency cannot be resolved.
@@ -457,8 +461,10 @@ Runner.prototype.getDependencies = function (module) {
       }
     }
   }
+
   return alldeps;
 }
+
 Runner.prototype.wrapper = function (func, arg) {
   thread = Components.classes["@mozilla.org/thread-manager;1"]
                      .getService(Components.interfaces.nsIThreadManager)
@@ -500,13 +506,13 @@ Runner.prototype.wrapper = function (func, arg) {
 
 Runner.prototype._runTestModule = function (module) {
   if (module.__requirements__ != undefined && module.__force_skip__ == undefined) {
-    for each(req in module.__requirements__) {
+    for each(var req in module.__requirements__) {
       module[req] = this.collector.getModule(req);
     }
   }
 
   var attrs = [];
-  for (i in module) {
+  for (var i in module) {
     attrs.push(i);
   }
   events.setModule(module);
@@ -521,7 +527,7 @@ Runner.prototype._runTestModule = function (module) {
     var setupModulePassed = true;
   }
   if (setupModulePassed) {
-    for (i in module.__tests__) {
+    for (var i in module.__tests__) {
       var test = module.__tests__[i];
       
       // TODO: introduce per-test timeout:
@@ -553,7 +559,7 @@ Runner.prototype._runTestModule = function (module) {
 
     }
   } else {
-    for each(test in module.__tests__) {
+    for each(var test in module.__tests__) {
       events.setTest(test);
       events.skip("setupModule failed.");
       events.endTest(test);
@@ -575,7 +581,7 @@ Runner.prototype.runTestModule = function (module) {
       }
     }
     var deps = this.getDependencies(module);
-    for (i in deps) {
+    for (var i in deps) {
       var dep = deps[i];
       if (dep.status != 'done') {
         this._runTestModule(dep);
