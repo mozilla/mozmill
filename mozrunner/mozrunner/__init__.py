95299f194a9e942000bc29d0098da9d27a1fd1b2
Jeff Hammel <jhammel@mozilla.com>
2010-09-13 11:52:45 -0700

    modify LD_LIBRARY_PATH on linux if the user specifies a path to firefox-bin instead of firefox, bug 575863

--- mozrunner/mozrunner/__init__.py
+++ mozrunner/mozrunner/__init__.py
@@ -334,9 +334,15 @@ class Runner(object):
         else:
             self.binary = binary
 
-
         if not os.path.exists(self.binary):
             raise Exception("Binary path does not exist "+self.binary)
+
+        if sys.platform == 'linux2' and self.binary.endswith('-bin'):
+            dirname = os.path.dirname(self.binary)
+            if os.environ.get('LD_LIBRARY_PATH', None):
+                os.environ['LD_LIBRARY_PATH'] = '%s:%s' % (os.environ['LD_LIBRARY_PATH'], dirname)
+            else:
+                os.environ['LD_LIBRARY_PATH'] = dirname
 
         self.profile = profile
 
