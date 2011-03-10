b32541f2324c07df80a66d649a6f2ada308f6ffd
Heather Arthur <fayearthur@gmail.com>
2011-03-09 16:35:26 -0800

    Bug 640352 - report_disconnect fails if self.current_test isnt set; r=jhammel

--- mozmill/mozmill/__init__.py
+++ mozmill/mozmill/__init__.py
@@ -265,7 +265,7 @@ class MozMill(object):
     ### methods for shutting down and cleanup
     
     def report_disconnect(self):
-        test = self.current_test
+        test = getattr(self, "current_test", {})
         test['passes'] = []
         test['fails'] = [{
           'exception' : {
