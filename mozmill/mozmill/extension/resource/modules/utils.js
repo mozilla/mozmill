21580329e302b2eec8b68fe87cdaf95d1f961ddb
Jeff Hammel <jhammel@mozilla.com>
2011-03-15 14:20:28 -0700

    Bug 640705 - remove commented out code from utils.js

--- mozmill/mozmill/extension/resource/modules/utils.js
+++ mozmill/mozmill/extension/resource/modules/utils.js
@@ -131,38 +131,6 @@ var checkChrome = function() {
    else { return false; }
 }
 
-/*var openFile = function(){
- const nsIFilePicker = Components.interfaces.nsIFilePicker;
-
- var fp = Components.classes["@mozilla.org/filepicker;1"].createInstance(nsIFilePicker);
- fp.init(window, "Select a Test Directory", nsIFilePicker.modeGetFolder);
-
- var rv = fp.show();
- if (rv == Components.interfaces.nsIFilePicker.returnOK){
-   // file is the given directory (nsIFile)
-   var array = [];
-   //iterate directories recursively
-   recurseDir = function(ent){
-       var entries = ent;
-       while(entries.hasMoreElements())
-       {
-         var entry = entries.getNext();
-         entry.QueryInterface(Components.interfaces.nsIFile);
-         if ((entry.isDirectory()) && (entry.path.indexOf('.svn') == -1)){
-           recurseDir(entry.directoryEntries);
-         }
-         //push js files onto the array
-         if (entry.path.indexOf('.js') != -1){
-           array.push(entry.path);
-         }
-       }
-   }
-   //build the files array
-   recurseDir(fp.file.directoryEntries);
-   paramObj = {};
-   paramObj.files = array;
-   mozmill.MozMillController.commands.jsTests(paramObj);
- }*/
  
  var runFile = function(w){
    //define the interface
@@ -180,11 +148,6 @@ var checkChrome = function() {
      var paramObj = {};
      paramObj.files = [];
      paramObj.files.push(thefile.path);
-
-     //Move focus to output tab
-     //w.document.getElementById('mmtabs').setAttribute("selectedIndex", 2);
-     //send it into the JS test framework to run the file
-     // jstest.runFromFile(thefile.path);
    }
  };
  
@@ -261,13 +224,7 @@ var checkChrome = function() {
       var thefile = fp.file;
       //create the paramObj with a files array attrib
       var data = getFile(thefile.path);
-      //w.document.getElementById('editorInput').value = data;
-      
-      //Move focus to output tab
-      //$('mmtabs').setAttribute("selectedIndex", 2);
-      //send it into the JS test framework to run the file
-      //mozmill.utils.jsTests(paramObj);
-      //jsTest.runFromString(thefile.path);
+
       return {path:thefile.path, data:data};
     }
   };
@@ -479,63 +436,3 @@ function waitForEval(expression, timeout, interval, subject) {
   return true;
 }
 
-
- // 
- // //Function to start the running of jsTests
- // var jsTests = function (paramObj) {
- //     //Setup needed variables
- //     mozmill.jsTest.actions.loadActions();
- //     var wm = mozmill.jsTest.actions;
- //     var testFiles = paramObj.files;
- //     if (!testFiles.length) {
- //       throw new Error('No JavaScript tests to run.');
- //     }
- //     var _j = mozmill.jsTest;
- //     //mozmill.MozMillController.stopLoop();
- // 
- //     //Timing the suite
- //     var jsSuiteSummary = new TimeObj();
- //     jsSuiteSummary.setName('jsSummary');
- //     jsSuiteSummary.startTime();
- //     _j.jsSuiteSummary = jsSuiteSummary;
- // 
- //     _j.run(paramObj);
- // };
- // 
- // //Commands function to hande the test results of the js tests
- // var jsTestResults = function () {
- //   var _j = mozmill.jsTest;
- //   var jsSuiteSummary = _j.jsSuiteSummary;
- //   var s = '';
- //   s += 'Number of tests run: ' + _j.testCount + '\n';
- //   s += '\nNumber of tests failures: ' + _j.testFailureCount;
- //   if (_j.testFailureCount > 0) {
- //     s += 'Test failures:<br/>';
- //     var fails = _j.testFailures;
- //     for (var i = 0; i < fails.length; i++) {
- //       var fail = fails[i];
- //       var msg = fail.message;
- //       // Escape angle brackets for display in HTML
- //       msg = msg.replace(/</g, '&lt;');
- //       msg = msg.replace(/>/g, '&gt;');
- //       s += msg + '<br/>';
- //     }
- //   };
- // 
- //   jsSuiteSummary.endTime();
- //   var result = !(_j.testFailureCount > 0);
- // 
- //   if (result){
- //      mozmill.results.writeResult(s, 'lightgreen');
- //    }
- //    else{
- //      mozmill.results.writeResult(s, 'lightred');
- //    }
- //   //mozmill.results.writeResult(s);
- //   //We want the summary to have a concept of success/failure
- //   var result = !(_j.testFailureCount > 0);
- //   var method = 'JS Test Suite Completion';
- //   //mozmill.jsTest.sendJSReport(method, result, null, jsSuiteSummary);
- //   // Fire the polling loop back up
- //   //mozmill.MozMillController.continueLoop();
- // }; 
