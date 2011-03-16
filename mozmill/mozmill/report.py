81e14ef3f80f1097ab763ec36ad1ee480232216e
Jeff Hammel <jhammel@mozilla.com>
2011-03-16 12:13:31 -0700

    include a (dummy) report type: mozmill-test

--- mozmill/mozmill/report.py
+++ mozmill/mozmill/report.py
@@ -75,7 +75,8 @@ class Report(object):
   def get_report(self, results):
     """get the report results"""
 
-    report = {'mozmill_version': results.mozmill_version,
+    report = {'report_type': 'mozmill-test',
+              'mozmill_version': results.mozmill_version,
               'time_start': results.starttime.strftime(self.date_format),
               'time_end': results.endtime.strftime(self.date_format),
               'time_upload': 'n/a',
