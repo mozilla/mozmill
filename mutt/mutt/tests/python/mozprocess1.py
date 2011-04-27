40975954f945eb9f0c5c8306e0bfeab7e57cb8ac
Clint Talbert <ctalbert@mozilla.com>
2011-04-27 13:51:07 -0700

    getting tests to work in ffx

--- mutt/mutt/tests/python/mozprocess1.py
+++ mutt/mutt/tests/python/mozprocess1.py
@@ -11,18 +11,29 @@ class ProcTest1(unittest.TestCase):
         CHROMEPATH = ["C:\\Users\\ctalbert\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe"]
         args = ["http://www.mozilla.org", "http://www.wikipedia.org"]
         cmd = CHROMEPATH + args
-        kp_kwargs = None
-        p = killableprocess.run_command(cmd, env = os.environ, **kp_kwargs)
+        kp_kwargs = {}
+        p = killableprocess.runCommand(cmd, env = os.environ, **kp_kwargs)
+        return p
+
+    def startffx(self):
+        FFXPATH = ["C:\\Program Files (x86)\\Minefield\\firefox.exe"]
+        args = ["--no-remote", "http://youtube.com"]
+        cmd = FFXPATH + args
+        kp_kwargs = {}
+        p = killableprocess.runCommand(cmd, env = os.environ, **kp_kwargs)
         return p
 
     def test_multiprockill(self):
-        p = self.startchrome()
-        sleep(15)
+        #p = self.startchrome()
+        p = self.startffx()
+        sleep(60)
         p.kill()
 
     def test_multiwaittimeout(self):
-        p = self.startchrome()
-        p.wait(timeout=30)
+        print "STARTING WAIT TEST"
+        #p = self.startchrome()
+        p = self.startffx()
+        p.wait(timeout=60)
         
 
 
