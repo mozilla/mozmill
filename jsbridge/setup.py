f54d5d5622eca31f01b0c6997b9254e079efc987
Clint Talbert <ctalbert@mozilla.com>
2011-05-12 15:25:08 -0700

    Bug 652846 version bump to 1.5.4pre r=jhammel

--- jsbridge/setup.py
+++ jsbridge/setup.py
@@ -42,9 +42,9 @@ desc = """Python to JavaScript bridge interface."""
 summ = """A powerful and extensible Python to JavaScript bridge interface."""
 
 PACKAGE_NAME = "jsbridge"
-PACKAGE_VERSION = "2.4.3"
+PACKAGE_VERSION = "2.4.4pre"
 
-requires = ['mozrunner == 2.5.4']
+requires = ['mozrunner == 2.5.5pre']
 
 if not sys.version.startswith('2.6'):
     requires.append('simplejson')
