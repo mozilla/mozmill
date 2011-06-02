c8b87d7fb87614cba3fa80eb54300f304c909902
Heather Arthur <fayearthur@gmail.com>
2011-06-02 11:11:59 -0700

    Bug 661588 - Don't set XUL cache prefs

--- mozmill/mozmill/extension/defaults/preferences/debug.js
+++ mozmill/mozmill/extension/defaults/preferences/debug.js
@@ -1,5 +1,3 @@
 /* debugging prefs */
 pref("browser.dom.window.dump.enabled", true);
 pref("javascript.options.showInConsole", true);
-pref("nglayout.debug.disable_xul_cache", true);
-pref("nglayout.debug.disable_xul_fastload", true);
