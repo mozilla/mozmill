5fce9044a3ff068e668185b5db62f9d0fb937eb8
Jeff Hammel <jhammel@mozilla.com>
2011-08-04 12:59:24 -0700

    move get_metadata to utils, it is not a core function

--- mozrunner/mozrunner/utils.py
+++ mozrunner/mozrunner/utils.py
@@ -43,13 +43,48 @@
 utility functions for mozrunner
 """
 
-__all__ = ['findInPath']
+__all__ = ['findInPath', 'get_metadata_from_egg']
 
 import mozinfo
 import os
 import sys
 
+
+### python package method metadata by introspection
+try:
+    import pkg_resources
+    def get_metadata_from_egg(module):
+        ret = {}
+        dist = pkg_resources.get_distribution(module)
+        if dist.has_metadata("PKG-INFO"):
+            key = None
+            for line in dist.get_metadata("PKG-INFO").splitlines():
+                # see http://www.python.org/dev/peps/pep-0314/
+                if key == 'Description':
+                    # descriptions can be long
+                    if not line or line[0].isspace():
+                        value += '\n' + line
+                        continue
+                    else:
+                        key = key.strip()
+                        value = value.strip()
+                        ret[key] = value
+
+                key, value = line.split(':', 1)
+                key = key.strip()
+                value = value.strip()
+                ret[key] = value
+        if dist.has_metadata("requires.txt"):
+            ret["Dependencies"] = "\n" + dist.get_metadata("requires.txt")    
+        return ret
+except ImportError:
+    # package resources not avaialable
+    def get_metadata_from_egg(module):
+        return {}
+
+
 def findInPath(fileName, path=os.environ['PATH']):
+    """python equivalent of which; should really be in the stdlib"""
     dirs = path.split(os.pathsep)
     for dir in dirs:
         if os.path.isfile(os.path.join(dir, fileName)):
