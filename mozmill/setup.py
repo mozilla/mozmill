7e526c9481942b1bfc4ac8b7b10b523e3e599ec5
Clint Talbert <ctalbert@mozilla.com>
2011-08-08 16:52:56 -0700

    Bug 676234 Fix toolkit and prepare for final 1.5.4 versions r=jhammel

--- mozmill/setup.py
+++ mozmill/setup.py
@@ -41,7 +41,7 @@ desc = """UI Automation tool for Mozilla applications."""
 summ = """A tool for full UI automation of Mozilla applications."""
 
 PACKAGE_NAME = "mozmill"
-PACKAGE_VERSION = "1.5.4b6"
+PACKAGE_VERSION = "1.5.4b7"
 
 setup(name=PACKAGE_NAME,
       version=PACKAGE_VERSION,
@@ -62,8 +62,8 @@ setup(name=PACKAGE_NAME,
           mozmill-restart = mozmill:restart_cli
         """,
       platforms =['Any'],
-      install_requires = ['jsbridge == 2.4.4b6',
-                          'mozrunner == 2.5.5b6',
+      install_requires = ['jsbridge == 2.4.4b7',
+                          'mozrunner == 2.5.5b7',
                           'ManifestDestiny == 0.2.2'],
       classifiers=['Development Status :: 4 - Beta',
                    'Environment :: Console',
