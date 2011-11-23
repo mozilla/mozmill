ba4dc0b004a2eb7aa17a1e8d2c338b19697187a6
Jeff Hammel <jhammel@mozilla.com>
2011-11-23 09:40:39 -0800

    Bug 704342] bump versions for hotfix-1.5 and release Mozmill 1.5.7 to pypi;r=whimboo

--- mozrunner/setup.py
+++ mozrunner/setup.py
@@ -41,7 +41,7 @@ import sys
 desc = """Reliable start/stop/configuration of Mozilla Applications (Firefox, Thunderbird, etc.)"""
 
 PACKAGE_NAME = "mozrunner"
-PACKAGE_VERSION = "2.5.7"
+PACKAGE_VERSION = "2.5.8"
 
 deps = []
 
