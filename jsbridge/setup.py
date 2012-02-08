21887b4dc0606bee7a7cb8fbeb32ced856955114
Jeff Hammel <jhammel@mozilla.com>
2012-02-08 10:41:55 -0800

    Bug 723296 - Release Mozmill 1.5.9;r=ctalbert

--- jsbridge/setup.py
+++ jsbridge/setup.py
@@ -42,9 +42,9 @@ desc = """Python to JavaScript bridge interface."""
 summ = """A powerful and extensible Python to JavaScript bridge interface."""
 
 PACKAGE_NAME = "jsbridge"
-PACKAGE_VERSION = "2.4.8"
+PACKAGE_VERSION = "2.4.9"
 
-requires = ['mozrunner == 2.5.9']
+requires = ['mozrunner == 2.5.10']
 
 if not sys.version.startswith('2.6'):
     requires.append('simplejson')
