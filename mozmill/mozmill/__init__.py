6112b3b1e410b377f415f77054a01fd3fbda6bee
David Guo <david.guo@uwaterloo.ca>
2012-04-12 00:34:05 -0700

    Bug 727660: Allows mozmill to use the http_proxy environment variable

--- mozmill/mozmill/__init__.py
+++ mozmill/mozmill/__init__.py
@@ -38,14 +38,12 @@
 # ***** END LICENSE BLOCK *****
 
 import copy
-import httplib
 import imp
 import os
 import socket
 import sys
 import traceback
-import urllib
-import urlparse
+import urllib2
 
 from datetime import datetime, timedelta
 import manifestparser
@@ -452,16 +450,13 @@ class MozMill(object):
             now = datetime.utcnow()
             results['time_upload'] = now.strftime("%Y-%m-%dT%H:%M:%SZ")
 
-            # Parse URL fragments and send data
-            url_fragments = urlparse.urlparse(report_url)
-            connection = httplib.HTTPConnection(url_fragments.netloc)
-            connection.request("POST", url_fragments.path, json.dumps(results),
-                               {"Content-type": "application/json"})
-        
+            # Send a POST request to the DB; the POST is implied by the body data
+            body = json.dumps(results)
+            request = urllib2.Request(report_url, body, {"Content-Type": "application/json"})
+
             # Get response which contains the id of the new document
-            response = connection.getresponse()
+            response = urllib2.urlopen(request)
             data = json.loads(response.read())
-            connection.close()
 
             # Check if the report has been created
             if not 'ok' in data:
@@ -470,6 +465,8 @@ class MozMill(object):
             # Print document location to the console and return
             print "Report document created at '%s%s'" % (report_url, data['id'])
             return data
+        except urllib2.HTTPError, e:
+            print e.read()
         except Exception, e:
             print "Sending results to '%s' failed (%s)." % (report_url, e)
 
