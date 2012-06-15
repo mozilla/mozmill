351667a0ed22d12efe83a331dbf6753add97711f
Henrik Skupin <mail@hskupin.info>
2012-06-15 21:11:19 +0200

    Bug 765086 - Allow usershutdown also from any setup/teardown methods. r=ahal

--- mozmill/test/restart/test_user_restart/test6.js
+++ mozmill/test/restart/test_user_restart/test6.js
@@ -1,8 +1,6 @@
 var setupModule = function () {
   controller = mozmill.getBrowserController();
-}
 
-var teardownModule = function () {
   controller.startUserShutdown(1000, true);
   controller.window.Application.restart();
 }
