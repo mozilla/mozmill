1857123687a50be1e9e067e9a3130b3bdf459b80
Henrik Skupin <mail@hskupin.info>
2012-06-21 10:11:44 +0200

    Bug 764442 - Application disconnect errors because of invalid frame objects if a test file restarts the browser - Part II. r=jhammel

--- mutt/mutt/tests/js/frame/restart/application_disconnect/test2.js
+++ mutt/mutt/tests/js/frame/restart/application_disconnect/test2.js
@@ -2,13 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// Bug 764442
+// Application disconnect errors because of invalid frame objects
+// if a test file restarts the browser
+
 var setupTest = function () {
   controller = mozmill.getBrowserController();
 }
 
 var testOne = function () {
-  controller.restartApplication('testTwo', true);
-}
-
-var testTwo = function () {
 }
