778b12ee6ca263dcc1def70906d5f39298ba5099
Henrik Skupin <mail@hskupin.info>
2012-08-09 15:23:23 +0200

    Re-enable test for fail and add test for skip

--- mutt/mutt/tests/python/usemozmill.py
+++ mutt/mutt/tests/python/usemozmill.py
@@ -8,9 +8,12 @@ class ModuleTest(unittest.TestCase):
     def test_modules(self):
         testpath = os.path.join("js-tests", "test_module1.js")
         self.do_test(testpath, passes=1, fails=0, skips=0)
+
         testpath = os.path.join("js-tests", "test_module2.js")
-        # TODO: Uncomment this when bug 761564 is fixed
-        #self.do_test(testpath, passes=0, fails=0, skips=1)
+        self.do_test(testpath, passes=0, fails=1, skips=0)
+
+        testpath = os.path.join("js-tests", "test_module3.js")
+        self.do_test(testpath, passes=0, fails=0, skips=1)
 
     def do_test(self, relative_test_path, passes=0, fails=0, skips=0):
         testpath = os.path.join(os.path.dirname(os.path.abspath(__file__)),
