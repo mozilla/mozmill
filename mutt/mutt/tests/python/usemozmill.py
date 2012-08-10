8dd5c6931e2ed311cf8e20265a7a3929481e2552
Henrik Skupin <mail@hskupin.info>
2012-08-10 19:51:53 +0200

    Bug 761564 - Remove logger usage in usemozmill.py. r=jhammel

--- mutt/mutt/tests/python/usemozmill.py
+++ mutt/mutt/tests/python/usemozmill.py
@@ -2,28 +2,27 @@ import os
 import tempfile
 import unittest
 import mozmill
-from mozmill.logger import LoggerListener
 
 
 class ModuleTest(unittest.TestCase):
     def test_modules(self):
         testpath = os.path.join("js-tests", "test_module1.js")
         self.do_test(testpath, passes=1, fails=0, skips=0)
+
         testpath = os.path.join("js-tests", "test_module2.js")
-        # TODO: Uncomment this when bug 761564 is fixed
-        #self.do_test(testpath, passes=0, fails=0, skips=1)
+        self.do_test(testpath, passes=0, fails=1, skips=0)
+
+        testpath = os.path.join("js-tests", "test_module3.js")
+        self.do_test(testpath, passes=0, fails=0, skips=1)
 
     def do_test(self, relative_test_path, passes=0, fails=0, skips=0):
         testpath = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                 relative_test_path)
         tests = [{'path': testpath}]
-        log_file = tempfile.mktemp(suffix='.txt')
-        logger = LoggerListener(log_file=log_file,
-                                file_level="DEBUG", debug=True)
-        m = mozmill.MozMill.create(handlers=(logger,),
-                                   runner_args={'cmdargs': ['-console']})
+
+        m = mozmill.MozMill.create(runner_args={'cmdargs': ['-console']})
         results = m.run(tests)
-        results.finish((logger,))
+        results.finish(())
 
         # From the first test, there is one passing test
         self.assertEqual(len(results.passes), passes, "Passes should match")
