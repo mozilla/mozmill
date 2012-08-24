5b51571badf1b0c7d61e3b88f3fe046da53dd5df
Henrik Skupin <mail@hskupin.info>
2012-08-24 11:53:39 +0200

    Rename components/mozmill.js to components/handlers.js and remove unecessary code

--- mozmill/mozmill/extension/content/mozmill.js
+++ mozmill/mozmill/extension/content/mozmill.js
@@ -2,7 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var mozmill = {}; Components.utils.import('resource://mozmill/driver/mozmill.js', mozmill);
 var utils = {}; Components.utils.import('resource://mozmill/stdlib/utils.js', utils);
 
 var updateOutput = function(){
--- mozmill/mozmill/extension/content/overlay.js
+++ mozmill/mozmill/extension/content/overlay.js
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var mozmillInit = {}; Components.utils.import('resource://mozmill/driver/mozmill.js', mozmillInit);
-
 var MozMill = {
   onLoad: function() {
     // initialization code
