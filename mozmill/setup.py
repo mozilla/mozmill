a137ba545fe010a7fe36c1e5765d5b110e948020
Henrik Skupin <mail@hskupin.info>
2012-08-25 08:23:44 +0200

    Bug 785442 - release jsbridge==2.4.15 and mozmill=1.5.18. r=jhammel

--- mozmill/setup.py
+++ mozmill/setup.py
@@ -41,7 +41,7 @@ desc = """UI Automation tool for Mozilla applications."""
 summ = """A tool for full UI automation of Mozilla applications."""
 
 PACKAGE_NAME = "mozmill"
-PACKAGE_VERSION = "1.5.17"
+PACKAGE_VERSION = "1.5.18"
 
 setup(name=PACKAGE_NAME,
       version=PACKAGE_VERSION,
@@ -62,7 +62,7 @@ setup(name=PACKAGE_NAME,
           mozmill-restart = mozmill:restart_cli
         """,
       platforms =['Any'],
-      install_requires = ['jsbridge == 2.4.14',
+      install_requires = ['jsbridge == 2.4.15',
                           'mozrunner == 2.5.13',
                           'ManifestDestiny == 0.2.2'],
       classifiers=['Development Status :: 4 - Beta',
