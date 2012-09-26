f75219cb3bc3e7aef9254903bf4af98c14ae8966
Szabolcs Hubai <szab.hu@gmail.com>
2012-09-26 23:44:15 +0200

    Bug 783223 - addressed comments, part 5: - kill setupModule

--- mutt/mutt/tests/js/utils/savescreenshot.js
+++ mutt/mutt/tests/js/utils/savescreenshot.js
@@ -2,12 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var utils = {};
+var utils = {}; Cu.import('resource://mozmill/stdlib/utils.js', utils);
 
-
-var setupModule = function () {
-  Cu.import('resource://mozmill/stdlib/utils.js', utils); 
-}
 
 /**
  * testScreenshotSaveCorruption
