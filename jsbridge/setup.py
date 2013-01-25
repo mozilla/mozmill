16450ee46518e44962e221731a5f845442e5851f
Henrik Skupin <mail@hskupin.info>
2013-01-25 23:13:11 +0100

    Bug 834893 - Bump Mozmill version to 1.5.20. r=jhammel

--- jsbridge/setup.py
+++ jsbridge/setup.py
@@ -42,9 +42,9 @@ desc = """Python to JavaScript bridge interface."""
 summ = """A powerful and extensible Python to JavaScript bridge interface."""
 
 PACKAGE_NAME = "jsbridge"
-PACKAGE_VERSION = "2.4.15"
+PACKAGE_VERSION = "2.4.16"
 
-requires = ['mozrunner == 2.5.13']
+requires = ['mozrunner == 2.5.14']
 
 if not sys.version.startswith('2.6'):
     requires.append('simplejson')
