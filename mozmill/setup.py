3bc6e5ee77a33a7c0338f688fc193d9129c9b646
Henrik Skupin <mail@hskupin.info>
2013-04-30 11:08:15 +0200

    Bug 866752 - Bump jsbridge dependency to 3.0rc2

--- mozmill/setup.py
+++ mozmill/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 PACKAGE_NAME = "mozmill"
 PACKAGE_VERSION = "2.0rc2"
 
-deps = ['jsbridge == 3.0rc1',
+deps = ['jsbridge == 3.0rc2',
         'ManifestDestiny == 0.5.6',
         'mozinfo == 0.4',
         'mozrunner == 5.15',
