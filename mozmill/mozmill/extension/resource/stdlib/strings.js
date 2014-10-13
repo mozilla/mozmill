ae8d7b65e64d68ba97e5f50ccf2093bc084edfeb
Lisa Hewus Fresh <bugz42@gmail.com>
2014-10-13 14:07:39 -0700

    Bug 671705 - Add a utility function to escape strings for use in regexes. r=hskupin

--- mozmill/mozmill/extension/resource/stdlib/strings.js
+++ mozmill/mozmill/extension/resource/stdlib/strings.js
@@ -2,9 +2,24 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, you can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var EXPORTED_SYMBOLS = ['trim', 'vslice'];
+var EXPORTED_SYMBOLS = ['escapeRegEx', 'trim', 'vslice'];
 
 var arrays = {}; Components.utils.import('resource://mozmill/stdlib/arrays.js', arrays);
+
+var sRE = /[-[\]{}()*+?.,\\^$|#\s]/g;
+
+/**
+ * Helper function to use when including externally generated strings
+ * in a regular expression.
+ *
+ * @param {string} aString text to be escaped.
+ *
+ * @returns {string} New string with metacharacters escaped.
+ */
+
+function escapeRegEx(aString) {
+  return aString.replace(sRE, "\\$&");
+}
 
 function trim(str) {
   return (str.replace(/^[\s\xA0]+/, "").replace(/[\s\xA0]+$/, ""));
